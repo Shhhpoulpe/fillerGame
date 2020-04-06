db4da0f9a86914a4d1909d2c660518f142b5a1ee
Maxime jobard <max.jobard@hotmail.fr>
2020-04-06 13:28:59 +0200

    not tested

---
+++ src/main/java/Init.java
@@ -0,0 +1,59 @@
+package main.java;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+import javax.swing.ImageIcon;
+
+public class Init {
+
+    protected int x;
+    protected int y;
+    protected int width;
+    protected int height;
+    protected boolean visible;
+    protected Image image;
+
+    public Init(int x, int y) {
+        // initialisation des coordonnées
+        this.x = x;
+        this.y = y;
+        visible = true;
+    }
+
+    protected void getImageDimensions() {
+
+        width = image.getWidth(null);
+        height = image.getHeight(null);
+    }
+
+    protected void loadImage(String imageName) {
+
+        // class ImageIcon pour charger un icone à partir d'une image
+        ImageIcon ii = new ImageIcon(imageName);
+        image = ii.getImage();
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+}
